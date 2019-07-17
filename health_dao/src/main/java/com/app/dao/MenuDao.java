902c2a87eadd9be8a03ea402d04df57a92e4ff09
wyp <931952307@qq.com>
2019-07-17 17:13:35 +0800

    update menudao

--- health_dao/src/main/java/com/app/dao/MenuDao.java
+++ health_dao/src/main/java/com/app/dao/MenuDao.java
@@ -3,8 +3,6 @@ package com.app.dao;
 import com.app01.pojo.Menu;
 import com.github.pagehelper.Page;
 
-import org.apache.ibatis.annotations.Param;
-
 import java.util.List;
 
 import java.util.LinkedHashSet;
@@ -13,5 +11,18 @@ public interface MenuDao {
 
     LinkedHashSet<Menu> findMenusByRoleId(int roleId);
 
-    List<Menu> findMenusByMenuId(@Param("roleId") int roleId,@Param("parentMenuId") int parentMenuId);
+    List<Menu> findMenusByMenuId(int menuid);
+
+    void add(Menu menu);
+
+    Page<Menu> findPageByCondition(String queryString);
+
+    void deleteById(int id);
+
+    Menu findMenuById(int id) ;
+
+
+    void updateMenu(Menu menu);
+
+    Long findCountByMenuId(int id);
 }
